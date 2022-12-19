3d0ead38f92c1c0482e8cde65b62bdbd759ca198
Ensiyeh-emadi <110991336+Ensiyeh-emadi@users.noreply.github.com>
2022-12-20 00:36:16 +0330

    Ninja-1

---
+++ sandbox.js
@@ -0,0 +1,98 @@
+// alert('hello word');
+// console.log(1);
+let age = 25;
+let year = 2019;
+console.log(age, year);
+
+ age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);  
+
+
+console.log('hello , word ');
+let email = 'emadiensiyeh@gmail.com';
+console.log(email);
+
+let firstName = 'Ensiyeh';
+let lastName = 'Emadi';
+let fullName = firstName +' '+
+    lastName;
+console.log(fullName);
+
+
+console.log(fullName[0]);
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
+// let result = fullName.toLocaleLowerCase();
+// console.log(result, fullName);
+let index = email.indexOf('m');
+console.log(index);
+let ninjas = ['ensiyeh', 'moti',];
+// console.log(ninjas);
+// ninjas[1] = 'samiyeh';
+// console.log(ninjas[1]);
+// let ages = [22, 42, 30, 52];
+// console.log(ages[2]); 
+console.log(ninjas.length);
+console.log(ninjas);
+// let result = ninjas.join('*');
+// let result = ninjas.indexOf('moti');
+// let result = ninjas.concat(['sami', 'bami']);
+// let result = ninjas.pop();
+// let result = ninjas.push('behman');
+// console.log(result);
+
+let radius = 10;
+const pi = 3.14;
+// console.log(pi , radius);
+
+ // math oprators +,-,*,/,**,%
+// console.log(10 / 2);
+// let resultttt = radius % 3;
+// let resultttt = pi * radius ** 2;
+
+// order of operation - B I D M A S 
+// let resultttt = 5 * (10 - 3) ** 2;
+
+// console.log(resultttt)
+let likes = 10;
+// likes = likes +1;
+// likes++;
+// likes--;
+// likes += 10;
+// likes -=5;
+// likes *= 2;
+// likes /= 2;
+// console.log(likes);
+ // NaN = not a number
+// console.log(5 / 'hello');
+// let resultttt = 'the blog ' + likes + ' llke';
+// console.log(resultttt);
+
+const title = 'Best reads of 2019';
+const author = 'Merio';
+const likeee = 30;
+
+
+
+// let resultttt = 'The bloge called ' + title + ' by ' + author + ' has ' + likes + 'likes';
+// console.log(resultttt);
+
+//
+// let html = `
+// <h2>${title}</h2>
+// <p>By ${author}</p>
+// <span>This blog  has ${likes} likes</span>
+
+// `;
+// console.log(html);
+let sen= null;
+// console.log(sen, sen + 3, `the sen is ${sen}`);
+console.log(sen, sen + 3, `the sen is ${sen}`);
+    
